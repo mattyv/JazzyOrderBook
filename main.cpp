ce6055e83360ce4420f63f3de314142c2f3f1146
Blair Davidson <blair.joel.davidson@gmail.com>
2025-09-22 20:29:02 +1000

    CMake basic scaffolding

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <vector>
+
+template<typename T>
+class flat_order_book {
+
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
